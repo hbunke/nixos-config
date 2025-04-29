0a6f92763a1874a6694170ba452a64b144f04da2
Hendrik Bunke <bunke@hbxt.org>
2025-04-29 16:36:53 +0200

    move samba to separate file
    otherwise service will run on all machines ;-)

--- configuration.nix
+++ configuration.nix
@@ -11,7 +11,7 @@
       ./local.nix 
     ];
 
- 
+
 
  # Bootloader.
   boot.loader.systemd-boot.enable = true;
@@ -200,50 +200,7 @@
   services.openssh.enable = true;
 
 
-  # Samba; https://nixos.wiki/wiki/Samba
-  # https://gist.github.com/vy-let/a030c1079f09ecae4135aebf1e121ea6
-  services.samba = {
-  package = pkgs.samba4Full;
-  enable = true;
-  securityType = "user";
-  openFirewall = true;
-  settings = {
-    global = {
-      "workgroup" = "WORKGROUP";
-      "server string" = "yoda";
-      "netbios name" = "yoda";
-      "security" = "user";
-      #"use sendfile" = "yes";
-      #"max protocol" = "smb2";
-      # note: localhost is the ipv6 localhost ::1
-      "hosts allow" = "192.168.0. 127.0.0.1 localhost";
-      "hosts deny" = "0.0.0.0/0";
-      "guest account" = "nobody";
-      "map to guest" = "bad user";
-    };
-    "scan" = {
-      "path" = "/home/bunke/SCANS";
-      "browseable" = "yes";
-      "read only" = "no";
-      "guest ok" = "no";
-      "create mask" = "0644";
-      "directory mask" = "0755";
-      #"force user" = "bunke";
-      #"force group" = "groupname";
-    };
-  };
-};
-  services.avahi = {
-    publish.enable = true;
-    publish.userServices = true;
-    enable = true;
-    openFirewall = true;
-  };
-
-  services.samba-wsdd = {
-    enable = true;
-    openFirewall = true;
-};
+ 
   
 
 
---
+++ samba.nix
@@ -0,0 +1,51 @@
+# Samba; https://nixos.wiki/wiki/Samba
+# https://gist.github.com/vy-let/a030c1079f09ecae4135aebf1e121ea6
+
+# don't forget to add user: sudo smbpasswd -a bunke
+
+{ config, pkgs, ... }:
+
+{
+  services.samba = {
+    package = pkgs.samba4Full;
+    enable = true;
+    securityType = "user";
+    openFirewall = true;
+    settings = {
+      global = {
+        "workgroup" = "WORKGROUP";
+        "server string" = "yoda";
+        "netbios name" = "yoda";
+        "security" = "user";
+        #"use sendfile" = "yes";
+        #"max protocol" = "smb2";
+        # note: localhost is the ipv6 localhost ::1
+        "hosts allow" = "192.168.0. 127.0.0.1 localhost";
+        "hosts deny" = "0.0.0.0/0";
+        "guest account" = "nobody";
+        "map to guest" = "bad user";
+      };
+      "scan" = {
+        "path" = "/home/bunke/SCANS";
+        "browseable" = "yes";
+        "read only" = "no";
+        "guest ok" = "no";
+        "create mask" = "0644";
+        "directory mask" = "0755";
+        #"force user" = "bunke";
+        #"force group" = "groupname";
+      };
+    };
+  };
+  services.avahi = {
+    publish.enable = true;
+    publish.userServices = true;
+    enable = true;
+    openFirewall = true;
+  };
+
+  services.samba-wsdd = {
+    enable = true;
+    openFirewall = true;
+  };
+}
